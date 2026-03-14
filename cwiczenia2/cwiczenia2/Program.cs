84be7a257581f93e38383a27215fcdf448afa11a
Michal Fukowski <michal01f@gmail.com>
2026-03-14 21:32:08 +0100

    Added input reading

--- cwiczenia2/cwiczenia2/Program.cs
+++ cwiczenia2/cwiczenia2/Program.cs
@@ -4,6 +4,8 @@ class Program
 {
     static void Main()
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Podaj liczby: ");
+        string input = Console.ReadLine();
+        string[] numbers = input.Split(' ');
     }
 }
