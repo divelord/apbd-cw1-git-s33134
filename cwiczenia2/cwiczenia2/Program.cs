cef54bdbfa87c2ba6272470f07e4674b61f4640e
Michal Fukowski <michal01f@gmail.com>
2026-03-14 21:34:22 +0100

    Added error message for empty input

--- cwiczenia2/cwiczenia2/Program.cs
+++ cwiczenia2/cwiczenia2/Program.cs
@@ -6,6 +6,13 @@ class Program
     {
         Console.WriteLine("Podaj liczby: ");
         string input = Console.ReadLine();
+
+        if (string.IsNullOrEmpty(input))
+        {
+            Console.WriteLine("Brak danych");
+            return;
+        }
+        
         string[] numbers = input.Split(' ');
     }
 }
