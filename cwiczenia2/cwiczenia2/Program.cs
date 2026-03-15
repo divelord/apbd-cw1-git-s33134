3c2040edb230c7bec570bbde57d3be997ac894f9
Michal Fukowski <michal01f@gmail.com>
2026-03-15 10:27:31 +0100

    completed CalculateMin method

--- cwiczenia2/cwiczenia2/Program.cs
+++ cwiczenia2/cwiczenia2/Program.cs
@@ -80,6 +80,16 @@ class Program
 
     static int CalculateMin(int[] numbers)
     {
+        int min = numbers[0];
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            if (numbers[i] < min)
+            {
+                min = numbers[i];
+            }
+        }
         
+        return min;
     }
 }
