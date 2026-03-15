4c226afab30e13ca0d02ed601ae7388053786f6f
Michal Fukowski <michal01f@gmail.com>
2026-03-15 11:13:06 +0100

    Added welcome message to main branch

--- cwiczenia2/cwiczenia2/Program.cs
+++ cwiczenia2/cwiczenia2/Program.cs
@@ -4,6 +4,8 @@ class Program
 {
     static void Main()
     {
+        Console.WriteLine("Program obliczeniowy\n");
+
         Console.WriteLine("Podaj liczby, oddzielone spacja: ");
         string input = Console.ReadLine();
 
@@ -42,15 +44,12 @@ class Program
         }
         Console.WriteLine();
         
-        Console.WriteLine();
         double average = CalculateAverage(numbers);
         Console.WriteLine("Srednia: " + average);
         
-        Console.WriteLine();
         int max = CalculateMax(numbers);
         Console.WriteLine("Max: " + max);
         
-        Console.WriteLine();
         int min = CalculateMin(numbers);
         Console.WriteLine("Min: " + min);
     }
